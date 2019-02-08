d9ae5a8ce1f9440da70e274a50bc2825440a2679
Michael Kappel <mike@ns12.com>
2019-02-07 22:36:01 -0600

    Added seed files for P10 & P63404

--- Prime-Number-Generator-Console/Program.cs
+++ Prime-Number-Generator-Console/Program.cs
@@ -14,10 +14,11 @@ namespace Prime_Number_Generator_Console
 {
     class Program
     {
+        private static Int32 ThreadCount = 1;
+        private static Int32 SaveFrequency;
+        private static Number NextNumber;
+        private static Boolean AutoContinue = false;
         private static IMathEnvironment CurrentMathEnvironment = new CharMathEnvironment(true, true, true, true, true, true, false, false, false, false);
-        private static IList<String> SeedPrimes = new List<String>();
-        private static IList<String> SupplementalPrimes = new List<String>();
-        private static IList<String> FoundPrimes = new List<String>();
 
         private static String PrimesSeedFile;
         private static String PrimesSupplementalFile;
@@ -26,7 +27,7 @@ namespace Prime_Number_Generator_Console
 
         public static SieveOfEratosthenePrimeAlgorithm PrimeOperator = new SieveOfEratosthenePrimeAlgorithm((NumberSegments foundPrime) =>
         {
-            FoundPrimes.Add(CurrentMathEnvironment.ConvertToString(foundPrime));
+
         });
 
         public static IBasicMathAlgorithm GetBasicMath()
@@ -36,24 +37,27 @@ namespace Prime_Number_Generator_Console
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Variable Base Pime Number Console");
-
-
-            Console.WriteLine("Please Specify Environment");
-            Console.WriteLine("Enter base such as \"0123456789\" or just press enter for P63404");
-
-            var commandMathEnvironmentCommand = Console.ReadLine();
-            if (commandMathEnvironmentCommand != "")
+            if (!AutoContinue)
             {
-                CurrentMathEnvironment = new CharMathEnvironment(commandMathEnvironmentCommand);
-            }
+                Console.WriteLine("Welcome to the Variable Base Pime Number Console");
 
-            Console.WriteLine("Environment Base {0}", CurrentMathEnvironment.Base);
 
-            if (PrimesSeedFile == null)
-            {
+                Console.WriteLine("Please Specify Environment");
+                Console.WriteLine("Enter base such as \"0123456789\" or just press enter for P63404");
+
+                var commandMathEnvironmentCommand = Console.ReadLine();
+                if (commandMathEnvironmentCommand != "")
+                {
+                    CurrentMathEnvironment = new CharMathEnvironment(commandMathEnvironmentCommand);
+                }
+
+                Console.WriteLine("Environment Base {0}", CurrentMathEnvironment.Base);
+
+
                 Console.WriteLine("Please Specify Primes Seed File");
-                Console.WriteLine("Press enter to use Primes.p{0}", CurrentMathEnvironment.Base);
+                Console.WriteLine("     Press ENTER to use Primes.p{0}", CurrentMathEnvironment.Base);
+
+                IList<String> SeedPrimes = new List<String>();
 
                 var primesSeedFileCommand = Console.ReadLine();
                 if (primesSeedFileCommand == "")
@@ -78,50 +82,46 @@ namespace Prime_Number_Generator_Console
                     }
                 }
 
-            }
 
-            Console.WriteLine("LOADED {0} Seed Primes ", SeedPrimes.Count);
+                Console.WriteLine("LOADED {0} Seed Primes ", SeedPrimes.Count);
+                Console.WriteLine("Re-Check Seed Primes?");
+                Console.WriteLine("     Press ENTER to Skip");
 
+                String doCheckSeedsCommand = Console.ReadLine().ToUpper();
 
-            Console.WriteLine("Re-Check Seed Primes?");
-            String doCheckSeedsCommand = Console.ReadLine().ToUpper();
-
-            if (doCheckSeedsCommand.StartsWith("Y") || doCheckSeedsCommand.StartsWith("S"))
-            {
-                var iterativePrimeAlgorithm = new IterativePrimeAlgorithm(CurrentMathEnvironment);
-                var basicMath = new BasicMathAlgorithm();
-
-
-                var badPrimes = new List<String>();
-                Parallel.ForEach(SeedPrimes, new ParallelOptions() { MaxDegreeOfParallelism = -1 }, (String uncheckedPrime) =>
+                if (doCheckSeedsCommand.StartsWith("Y") || doCheckSeedsCommand.StartsWith("S"))
                 {
-                    if (!iterativePrimeAlgorithm.IsPrime(CurrentMathEnvironment, basicMath, CurrentMathEnvironment.ParseNumberSegments(uncheckedPrime)))
+                    var iterativePrimeAlgorithm = new IterativePrimeAlgorithm(CurrentMathEnvironment);
+                    var basicMath = new BasicMathAlgorithm();
+
+
+                    var badPrimes = new List<String>();
+                    Parallel.ForEach(SeedPrimes, new ParallelOptions() { MaxDegreeOfParallelism = -1 }, (String uncheckedPrime) =>
                     {
-                        lock (badPrimes)
+                        if (!iterativePrimeAlgorithm.IsPrime(CurrentMathEnvironment, basicMath, CurrentMathEnvironment.ParseNumberSegments(uncheckedPrime)))
                         {
+                            lock (badPrimes)
+                            {
 
-                            Console.Write(String.Format("Bad Seed Prime {0}", badPrimes.Count));
-                            badPrimes.Add(uncheckedPrime);
+                                Console.Write(String.Format("Bad Seed Prime {0}", badPrimes.Count));
+                                badPrimes.Add(uncheckedPrime);
+                            }
                         }
-                    }
-                    else
-                    {
-                        Console.WriteLine(uncheckedPrime);
-                    }
-                });
+                        else
+                        {
+                            Console.WriteLine(uncheckedPrime);
+                        }
+                    });
 
-                Console.WriteLine(String.Format("{0} Bad Seed Primes Found", badPrimes.Count));
-            }
+                    Console.WriteLine(String.Format("{0} Bad Seed Primes Found", badPrimes.Count));
+                }
 
-            PrimeOperator.LoadSeedPrimes(SeedPrimes.Select(x => CurrentMathEnvironment.ParseNumberSegments(x)).ToList(), CurrentMathEnvironment, GetBasicMath());
-            Console.WriteLine("Converted {0} Seed Primes ", SeedPrimes.Count);
+                Console.WriteLine("Converting... {0} Seed Primes ", SeedPrimes.Count);
+                PrimeOperator.LoadSeedPrimes(SeedPrimes.Select(x => CurrentMathEnvironment.ParseNumberSegments(x)).ToList(), CurrentMathEnvironment, GetBasicMath());
+                Console.WriteLine("Converted {0} Seed Primes ", SeedPrimes.Count);
 
-
-
-            if (PrimesSupplementalFile == null)
-            {
                 Console.WriteLine("Please Specify Primes Supplemental File");
-                Console.WriteLine("Press enter to use Supplemental.p{0}", CurrentMathEnvironment.Base);
+                Console.WriteLine("     Press ENTER to use Supplemental.p{0}", CurrentMathEnvironment.Base);
 
                 var primesSupplementalFileCommand = Console.ReadLine();
                 if (primesSupplementalFileCommand == "")
@@ -135,7 +135,7 @@ namespace Prime_Number_Generator_Console
 
                 Console.WriteLine("LOADING: Supplemental Seed File {0}", CurrentMathEnvironment.Base);
 
-                SupplementalPrimes = new List<String>();
+                IList<String> SupplementalPrimes = new List<String>();
                 using (FileStream fs = File.Open(PrimesSupplementalFile, FileMode.OpenOrCreate))
                 {
                     using (StreamReader sr = new StreamReader(fs))
@@ -146,78 +146,87 @@ namespace Prime_Number_Generator_Console
                         }
                     }
                 }
-            }
 
-            Console.WriteLine("LOADED {0} Supplemental Primes ", SupplementalPrimes.Count);
-            PrimeOperator.LoadSupplementalPrimes(SupplementalPrimes.Select(x => CurrentMathEnvironment.ParseNumberSegments(x)).ToList());
-            Console.WriteLine("Converted {0} Supplemental Primes ", SupplementalPrimes.Count);
+                Console.WriteLine("LOADED {0} Supplemental Primes ", SupplementalPrimes.Count);
+                PrimeOperator.LoadSupplementalPrimes(SupplementalPrimes.Select(x => CurrentMathEnvironment.ParseNumberSegments(x)).ToList());
+                Console.WriteLine("Converted {0} Supplemental Primes ", SupplementalPrimes.Count);
 
-            Console.WriteLine("How often do you want to save?");
-            Console.WriteLine("Press enter for every {0} checks", CurrentMathEnvironment.Base);
+                Console.WriteLine("How many do you want to try?");
+                Console.WriteLine("    Press ENTER for {0}", CurrentMathEnvironment.Base);
 
-            Int32 saveFrequency = (Int32)CurrentMathEnvironment.Base;
-            String saveCommand = Console.ReadLine();
-            if (saveCommand != "")
-            {
-                saveFrequency = Int32.Parse(saveCommand);
-            }
-
-            Console.WriteLine("Number of Threads:");
-            Console.WriteLine("Press enter to use 1", CurrentMathEnvironment.Base);
-
-            String threadCommand = Console.ReadLine();
-            if (threadCommand == "")
-            {
-                threadCommand = "1";
-            }
-
-            Console.WriteLine("Start With Number");
-            Console.WriteLine("Press enter to use highest seed prime", CurrentMathEnvironment.Base);
-            String startWithCommand = Console.ReadLine();
-
-            Number firstNumber;
-            if (startWithCommand == "")
-            {
-                if (SeedPrimes.Count() == 0)
+                SaveFrequency = (Int32)CurrentMathEnvironment.Base;
+                String saveCommand = Console.ReadLine();
+                if (saveCommand != "")
                 {
-                    firstNumber = CurrentMathEnvironment.SecondNumber;
+                    SaveFrequency = Int32.Parse(saveCommand);
+                }
+
+                Console.WriteLine("Auto Continue?");
+                Console.WriteLine("    Press ENTER for Yes");
+
+                String autoContinueCommand = Console.ReadLine().ToUpper();
+                if (autoContinueCommand == "" || autoContinueCommand.StartsWith("Y"))
+                {
+                    AutoContinue = true;
+                }
+
+                Console.WriteLine("Number of Threads:");
+                Console.WriteLine("     Press ENTER to use 1", CurrentMathEnvironment.Base);
+
+                String threadCommand = Console.ReadLine();
+                if (!Int32.TryParse(threadCommand, out ThreadCount))
+                {
+                    ThreadCount = 1;
+                }
+
+                Console.WriteLine("Start With Number");
+                Console.WriteLine("     Press ENTER to use highest seed prime", CurrentMathEnvironment.Base);
+                String startWithCommand = Console.ReadLine();
+
+                Number firstNumber;
+                if (startWithCommand == "")
+                {
+                    if (SeedPrimes.Count() == 0)
+                    {
+                        firstNumber = CurrentMathEnvironment.SecondNumber;
+                    }
+                    else
+                    {
+                        firstNumber = CurrentMathEnvironment.GetNumber(SeedPrimes[SeedPrimes.Count - 1]);
+                    }
                 }
                 else
                 {
-                    firstNumber = CurrentMathEnvironment.GetNumber(SeedPrimes[SeedPrimes.Count - 1]);
+                    firstNumber = CurrentMathEnvironment.GetNumber(startWithCommand);
                 }
-            }
-            else
-            {
-                firstNumber = CurrentMathEnvironment.GetNumber(startWithCommand);
-            }
 
-            Console.WriteLine("Starting with: {0} ({1})", firstNumber, firstNumber.GetDecimalArray());
-            Console.WriteLine("At:{0}", DateTime.Now);
+                Console.WriteLine("*************************************");
+                Console.WriteLine("Starting with: {0} ({1})", firstNumber, firstNumber.GetDecimalArray());
+                Console.WriteLine("At:{0}", DateTime.Now);
 
-
-            if (PrimeOperator.IsPrime(firstNumber.Environment, GetBasicMath(), firstNumber.Segments))
-            {
-                Console.WriteLine("Prime:{0}", DateTime.Now);
+                if (PrimeOperator.IsPrime(firstNumber.Environment, GetBasicMath(), firstNumber.Segments))
+                {
+                    Console.WriteLine("Prime:{0}", DateTime.Now);
+                }
+                else
+                {
+                    Console.WriteLine("Not Prime:{0}", DateTime.Now);
+                }
+                Console.WriteLine("*************** GO ******************");
+                NextNumber = firstNumber + CurrentMathEnvironment.KeyNumber[1];
             }
-            else
-            {
-                Console.WriteLine("Not Prime:{0}", DateTime.Now);
-            }
-
-            Number nextNumber = firstNumber + CurrentMathEnvironment.KeyNumber[1];
 
             Boolean running = true;
             Int64 checkCount = 0;
             Action<Number> f = (testNumber) =>
             {
                 DateTime startTime = DateTime.Now;
-                if (PrimeOperator.IsPrime(firstNumber.Environment, GetBasicMath(), testNumber.Segments))
+                if (PrimeOperator.IsPrime(CurrentMathEnvironment, GetBasicMath(), testNumber.Segments))
                 {
                     Console.WriteLine("{0} ({1}) Th:{2} T:{3}", testNumber.GetCharArray(), testNumber.GetDecimalArray(), Thread.CurrentThread.ManagedThreadId, DateTime.Now - startTime);
                 }
                 checkCount += 1;
-                if (checkCount % saveFrequency == 0)
+                if (checkCount % SaveFrequency == 0)
                 {
                     running = false;
                 }
@@ -226,14 +235,14 @@ namespace Prime_Number_Generator_Console
             List<Task> tasks = new List<Task>();
             Task t = Task.Run(() =>
             {
-                for (var i = 0; i < Int32.Parse(threadCommand); i++)
+                for (var i = 0; i < ThreadCount; i++)
                 {
                     Task tItem = Task.Run(() =>
                     {
                         while (running)
                         {
-                            nextNumber = nextNumber + CurrentMathEnvironment.KeyNumber[1];
-                            f(nextNumber);
+                            NextNumber = NextNumber + CurrentMathEnvironment.KeyNumber[1];
+                            f(NextNumber);
                         }
                     });
                     tasks.Add(tItem);
@@ -267,50 +276,68 @@ namespace Prime_Number_Generator_Console
                         }
                     }
                 }
-                Console.WriteLine(String.Format("Completed With:{0}", nextNumber));
-            });
+                Console.WriteLine(String.Format("Completed With:{0}", NextNumber));
+                Console.WriteLine("*************** STOP ******************");
 
-
-            if (Console.ReadLine() == "")
-            {
-                running = false;
-            }
-
-
-            Console.WriteLine("Save?");
-            String doSaveCommand = Console.ReadLine().ToUpper();
-            if (doSaveCommand.StartsWith("Y") || doSaveCommand.StartsWith("S"))
-            {
-                var definitionFileName = String.Format("{0}/Definition.p{1}", BaseFolderPath, CurrentMathEnvironment.Base);
-                using (FileStream fs = File.Open(definitionFileName, FileMode.Create))
+                Boolean doSave = true;
+                if (!AutoContinue)
                 {
-                    using (StreamWriter sw = new StreamWriter(fs))
+                    if (Console.ReadLine() == "")
                     {
-                        sw.WriteLine(CurrentMathEnvironment.GetDefinition());
+                        running = false;
                     }
+
+
+                    Console.WriteLine("Save?");
+                    String doSaveCommand = Console.ReadLine().ToUpper();
+                    if (doSaveCommand.StartsWith("Y") || doSaveCommand.StartsWith("S"))
+                    {
+                        doSave = true;
+                    }
+                    else
+                    {
+                        doSave = false;
+                    }
+
+                    if (doSave)
+                    {
+                        var definitionFileName = String.Format("{0}/Definition.p{1}", BaseFolderPath, CurrentMathEnvironment.Base);
+                        using (FileStream fs = File.Open(definitionFileName, FileMode.Create))
+                        {
+                            using (StreamWriter sw = new StreamWriter(fs))
+                            {
+                                sw.WriteLine(CurrentMathEnvironment.GetDefinition());
+                            }
+                        }
+
+                        Console.WriteLine("Saved as {0}", definitionFileName);
+
+                    }
+
+                   
                 }
 
-                Console.WriteLine("Saved as {0}", definitionFileName);
-
-                var fileName = String.Format("{0}{1}.p{2}", BaseFolderPath, DateTime.Now.Ticks, CurrentMathEnvironment.Base);
-                using (FileStream fs = File.Open(fileName, FileMode.Create))
+                if (doSave)
                 {
-                    using (StreamWriter sw = new StreamWriter(fs))
+                    var fileName = String.Format("{0}{1}.p{2}", BaseFolderPath, DateTime.Now.Ticks, CurrentMathEnvironment.Base);
+                    using (FileStream fs = File.Open(fileName, FileMode.Create))
                     {
-                        foreach (NumberSegments prime in PrimeOperator.PrimeNumbers)
+                        using (StreamWriter sw = new StreamWriter(fs))
                         {
-                            sw.WriteLine(CurrentMathEnvironment.ConvertToString(prime));
+                            foreach (NumberSegments prime in PrimeOperator.PrimeNumbers)
+                            {
+                                sw.WriteLine(CurrentMathEnvironment.ConvertToString(prime));
+                            }
                         }
                     }
+
+                    Console.WriteLine("Saved as {0}", fileName);
                 }
 
-                Console.WriteLine("Saved as {0}", fileName);
+                Main(args);
+            });
 
-            }
-
-
-            Main(args);
+            Console.ReadLine();
         }
-
     }
 }
